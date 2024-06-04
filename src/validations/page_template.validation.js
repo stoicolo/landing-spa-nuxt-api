081a21b8804c23b18b46ec9e3cb394ee57e52cb4
francomac <franco.mcarballo@outlook.com>
2024-06-04 15:55:10 -0600

    create add pageTemplate validations

---
+++ src/validations/page_template.validation.js
@@ -0,0 +1,48 @@
+const Joi = require('joi');
+
+const createPageTemplate = {
+  body: Joi.object().keys({
+    pageTemplateId: Joi.number().required(),
+    sections: Joi.array().items(Joi.object()),
+  }),
+};
+
+const getPageTemplate = {
+  params: Joi.object().keys({
+    pageTemplateId: Joi.number().required(),
+  }),
+};
+
+const getPageTemplates = {
+  params: Joi.object().keys({
+    pageTemplateId: Joi.number(),
+  }),
+  query: Joi.object().keys({
+    sections: Joi.array().items(Joi.object()),
+  }),
+};
+
+const updatePageTemplate = {
+  params: Joi.object().keys({
+    pageTemplateId: Joi.number().required(),
+  }),
+  body: Joi.object()
+    .keys({
+      sections: Joi.array().items(Joi.object()),
+    })
+    .min(1),
+};
+
+const deledeletePageTemplate = {
+  params: Joi.object().keys({
+    pageTemplateId: Joi.number().required(),
+  }),
+};
+
+module.exports = {
+  createPageTemplate,
+  getPageTemplate,
+  getPageTemplates,
+  updatePageTemplate,
+  deledeletePageTemplate,
+};
