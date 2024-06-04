eac3b2a5f44e5536b1f92a7968cc38e70e686966
francomac <franco.mcarballo@outlook.com>
2024-06-04 15:54:12 -0600

    create add pageTemplate controller

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,4 +1,4 @@
 module.exports.authController = require('./auth.controller');
 module.exports.userController = require('./user.controller');
 module.exports.companyController = require('./company.controller');
-module.exports.quizController = require('./quiz.controller');
+module.exports.pageTemplateController = require('./page_template.controller');
