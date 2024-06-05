695a8a5c0f5c9d7ef697ebffab597a6632ac0a6c
francomac <franco.mcarballo@outlook.com>
2024-06-05 10:41:31 -0600

    adjusted pageTemplate model

--- src/models/page_template.model.js
+++ src/models/page_template.model.js
@@ -3,20 +3,29 @@
 const { DataTypes } = require('sequelize');
 const { sequelize } = require('../config/sequelize');
 
-const PageTemplate = sequelize.define('PageTemplate', {
-  sections: {
-    type: DataTypes.ARRAY(DataTypes.JSON),
-    allowNull: false,
-    defaultValue: [],
-  },
-  userId: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    references: {
-      model: 'User',
-      key: 'id',
+const PageTemplate = sequelize.define(
+  'PageTemplate',
+  {
+    sections: {
+      type: DataTypes.ARRAY(DataTypes.JSON),
+      allowNull: false,
+      defaultValue: [],
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'User',
+        key: 'id',
+      },
     },
   },
-});
+  {
+    timestamps: true,
+    sequelize, // We need to pass the `sequelize` instance
+    modelName: 'PageTemplate',
+    tableName: 'PageTemplate',
+  }
+);
 
 module.exports = PageTemplate;
