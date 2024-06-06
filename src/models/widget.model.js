23ecade50f765110e8364a2df22daf1ec4d7cfcd
francomac <franco.mcarballo@outlook.com>
2024-06-06 15:04:19 -0600

    create add widget model

--- src/models/widget.model.js
+++ src/models/widget.model.js
@@ -3,17 +3,41 @@
 const { DataTypes } = require('sequelize');
 const { sequelize } = require('../config/sequelize');
 
-const Widget = sequelize.define('Widget', {
-  name: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    trim: true,
+const Widget = sequelize.define(
+  'Widget',
+  {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      trim: true,
+    },
+    category: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      trim: true,
+    },
+    image: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      trim: true,
+    },
+    available: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      trim: true,
+    },
+    element: {
+      type: DataTypes.JSON,
+      allowNull: true,
+      defaultValue: {},
+    },
   },
-  element: {
-    type: DataTypes.JSON,
-    allowNull: true,
-    defaultValue: {},
-  },
-});
+  {
+    timestamps: true,
+    sequelize, // We need to pass the `sequelize` instance
+    modelName: 'Widget',
+    tableName: 'Widget',
+  }
+);
 
 module.exports = Widget;
