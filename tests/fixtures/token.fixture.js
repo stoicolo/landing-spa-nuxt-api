b210f3bb78f29df73f266861e09f806cfdc9e262
francomac <franco.mcarballo@outlook.com>
2024-06-19 17:51:09 -0600

    moment removed from fixtures

--- tests/fixtures/token.fixture.js
+++ tests/fixtures/token.fixture.js
@@ -1,10 +1,10 @@
-const moment = require('moment');
+const dayjs = require('dayjs');
 const config = require('../../src/config/config');
 const { tokenTypes } = require('../../src/config/tokens');
 const tokenService = require('../../src/services/token.service');
 const { userOne, admin } = require('./user.fixture');
 
-const accessTokenExpires = moment().add(config.jwt.accessExpirationMinutes, 'minutes');
+const accessTokenExpires = dayjs().add(config.jwt.accessExpirationMinutes, 'minutes');
 const userOneAccessToken = tokenService.generateToken(userOne._id, accessTokenExpires, tokenTypes.ACCESS);
 const adminAccessToken = tokenService.generateToken(admin._id, accessTokenExpires, tokenTypes.ACCESS);
 
