6648373ffa4f0ad3c1c0b64ee0878517e29ceebc
francomac <franco.mcarballo@outlook.com>
2024-07-01 21:36:49 -0600

    feature(Website): New model

---
+++ src/models/website.model.js
@@ -0,0 +1,32 @@
+const { DataTypes } = require('sequelize');
+const { sequelize } = require('../config/sequelize');
+
+const Website = sequelize.define(
+  'Website',
+  {
+    pageId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'Page',
+        key: 'id',
+      },
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'User',
+        key: 'id',
+      },
+    },
+  },
+  {
+    timestamps: true,
+    sequelize, // We need to pass the `sequelize` instance
+    modelName: 'Website',
+    tableName: 'Website',
+  }
+);
+
+module.exports = Website;
