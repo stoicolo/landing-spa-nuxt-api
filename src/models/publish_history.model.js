e92b1d93a5bd90a6578250de16c4bd0e35dde648
francomac <franco.mcarballo@outlook.com>
2024-07-15 21:14:54 -0600

    feature(PublishHistory): new Model

---
+++ src/models/publish_history.model.js
@@ -0,0 +1,55 @@
+// PageTemplate.js
+
+const { DataTypes } = require('sequelize');
+const { sequelize } = require('../config/sequelize');
+
+const PublishHistory = sequelize.define(
+  'PublishHistory',
+  {
+    pageName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    websiteId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'Website',
+        key: 'id',
+      },
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'User',
+        key: 'id',
+      },
+    },
+    menuHeaderId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'PageTemplate',
+        key: 'id',
+      },
+    },
+    isActive: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+    },
+    isPublic: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+    },
+  },
+  {
+    timestamps: true,
+    sequelize, // We need to pass the `sequelize` instance
+    modelName: 'PublishHistory',
+    tableName: 'PublishHistory',
+  }
+);
+
+module.exports = PublishHistory;
