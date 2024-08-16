67826e5604766d6838ca9d65b23bbfd48cc760f8
francomac <franco.mcarballo@outlook.com>
2024-08-15 22:07:10 -0600

    Sequelize config SSL adjusted

--- src/config/sequelize.js
+++ src/config/sequelize.js
@@ -1,5 +1,21 @@
 // sequelize config and Postgres database connection
 
+// const { Sequelize } = require('sequelize');
+// const config = require('./config');
+
+// const sequelize = new Sequelize({
+//   dialect: config.database.dialect,
+//   host: config.database.host,
+//   database: config.database.name,
+//   adminDatabase: config.database.adminUser,
+//   username: config.database.username,
+//   password: config.database.password,
+//   port: config.database.port,
+//   ssl: config.database.ssl,
+// });
+
+// module.exports = { sequelize };
+
 const { Sequelize } = require('sequelize');
 const config = require('./config');
 
@@ -7,11 +23,16 @@ const sequelize = new Sequelize({
   dialect: config.database.dialect,
   host: config.database.host,
   database: config.database.name,
-  adminDatabase: config.database.adminUser,
   username: config.database.username,
   password: config.database.password,
   port: config.database.port,
-  ssl: config.database.ssl,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false, // Utilice esto si tiene problemas con certificados autofirmados
+    },
+  },
+  logging: console.log, // Esto ayudará a depurar problemas de conexión
 });
 
 module.exports = { sequelize };
