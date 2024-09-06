6a583236e84fa79b2739f1263db5f6d6bdc29f03
francomac <franco.mcarballo@outlook.com>
2024-09-05 20:22:17 -0600

    feature(Sequilize): Make Sequelize Configurable based on NODE_ENV

--- src/config/sequelize.js
+++ src/config/sequelize.js
@@ -1,40 +1,39 @@
-// sequelize config and Postgres database connection
-
-// DEVELOPMENT
-// const { Sequelize } = require('sequelize');
-// const config = require('./config');
-
-// const sequelize = new Sequelize({
-//   dialect: config.database.dialect,
-//   host: config.database.host,
-//   database: config.database.name,
-//   adminDatabase: config.database.adminUser,
-//   username: config.database.username,
-//   password: config.database.password,
-//   port: config.database.port,
-//   ssl: config.database.ssl,
-// });
-
-// module.exports = { sequelize };
-
-// PRODUCTION
 const { Sequelize } = require('sequelize');
 const config = require('./config');
 
-const sequelize = new Sequelize({
-  dialect: config.database.dialect,
-  host: config.database.host,
-  database: config.database.name,
-  username: config.database.username,
-  password: config.database.password,
-  port: config.database.port,
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false, // Utilice esto si tiene problemas con certificados autofirmados
+const environment = config.node_env || 'development';
+
+let sequelizeConfig;
+
+if (environment === 'production') {
+  sequelizeConfig = {
+    dialect: config.database.dialect,
+    host: config.database.host,
+    database: config.database.name,
+    username: config.database.username,
+    password: config.database.password,
+    port: config.database.port,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
     },
-  },
-  logging: console.log, // Esto ayudará a depurar problemas de conexión
-});
+    logging: console.log,
+  };
+} else {
+  sequelizeConfig = {
+    dialect: config.database.dialect,
+    host: config.database.host,
+    database: config.database.name,
+    adminDatabase: config.database.adminUser,
+    username: config.database.username,
+    password: config.database.password,
+    port: config.database.port,
+    ssl: config.database.ssl,
+  };
+}
+
+const sequelize = new Sequelize(sequelizeConfig);
 
 module.exports = { sequelize };
