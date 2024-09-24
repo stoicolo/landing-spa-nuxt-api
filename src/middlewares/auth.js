4ebfc83e6c8b409b6347d1d75352c7fac724c9b1
francomac <franco.mcarballo@outlook.com>
2024-09-24 17:15:37 -0600

    feature(Auth): Changes in middleware for user interactions

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -2,13 +2,13 @@ const passport = require('passport');
 const httpStatus = require('http-status');
 const ApiError = require('../utils/ApiError');
 const { roleRights } = require('../config/roles');
+const logger = require('../config/logger');
 
-// Authentication verification
+// Authentication verification callback
 const verifyCallback = (req, resolve, reject, requiredRights) => async (err, user, info) => {
-  // Check for error, info, or unauthenticated user
-
   if (err || info || !user) {
-    return reject(new ApiError(httpStatus.UNAUTHORIZED, 'Por favor autenticarse para continuar.'));
+    logger.warn(`Fallo de autenticación: ${err?.message || info?.message || 'Usuario no encontrado'}`);
+    return reject(new ApiError(httpStatus.UNAUTHORIZED, 'Por favor, autentíquese para continuar.'));
   }
 
   req.user = user;
@@ -18,33 +18,26 @@ const verifyCallback = (req, resolve, reject, requiredRights) => async (err, use
     const userRights = roleRights.get(user.role);
     const hasRequiredRights = requiredRights.every((requiredRight) => userRights.includes(requiredRight));
     if (!hasRequiredRights && req.params.userId !== user.id) {
-      return reject(new ApiError(httpStatus.FORBIDDEN, 'Acceso denegado, no tienes los permisos necesarios.'));
+      logger.warn(`Intento de acceso no autorizado por el usuario ${user.id} a la ruta ${req.originalUrl}`);
+      return reject(new ApiError(httpStatus.FORBIDDEN, 'Acceso denegado. No tiene los permisos necesarios.'));
     }
   }
 
   resolve();
 };
 
-// Authentication middleware
 const auth =
   (...requiredRights) =>
   async (req, res, next) => {
-    try {
-      // Authenticate the request using Passport
-      await new Promise((resolve, reject) => {
-        passport.authenticate('CC_Strategy_jwt', { session: false }, verifyCallback(req, resolve, reject, requiredRights))(
-          req,
-          res,
-          next
-        );
-      });
-
-      // If authenticated successfully, move to the next middleware
-      next();
-    } catch (err) {
-      // Capture any error and pass it to the next error handling middleware
-      next(err);
-    }
+    return new Promise((resolve, reject) => {
+      passport.authenticate('CC_Strategy_jwt', { session: false }, verifyCallback(req, resolve, reject, requiredRights))(
+        req,
+        res,
+        next
+      );
+    })
+      .then(() => next())
+      .catch((err) => next(err));
   };
 
 module.exports = auth;
