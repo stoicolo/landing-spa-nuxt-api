7e5723da730cae9b21ea02292814fa1ef2f383b0
francomac <franco.mcarballo@outlook.com>
2024-09-27 19:18:29 -0600

    fix(PulicWebhook): validation adjusted to get string o null

--- src/validations/public_webhook.validation.js
+++ src/validations/public_webhook.validation.js
@@ -6,12 +6,12 @@ const successfulSubscription = {
     email: Joi.string().required(),
     modality: Joi.string().required(),
     amount: Joi.number().required(),
-    coupon: Joi.string(),
+    coupon: Joi.alternatives().try(Joi.string(), Joi.valid(null)),
     free_trial: Joi.number().required(),
     next_payment_date: Joi.string().required(),
     paymentId: Joi.number().required(),
     auth: Joi.number().required(),
-    frequency: Joi.string(),
+    frequency: Joi.alternatives().try(Joi.string(), Joi.valid(null)),
   }),
 };
 
