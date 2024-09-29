1adc71df54a4461023a46dcbfce1f370dcc96292
francomac <franco.mcarballo@outlook.com>
2024-09-29 17:26:34 -0600

    feature(PulicWebhook): new service for payment failed

--- src/services/public_webhook.service.js
+++ src/services/public_webhook.service.js
@@ -1,5 +1,5 @@
 const httpStatus = require('http-status');
-const { PublicWebhookPayments, PublicWebhookSubscriptions } = require('../models');
+const { PublicWebhookPayments, PublicWebhookSubscriptions, PublicWebhookPaymentFailed } = require('../models');
 const ApiError = require('../utils/ApiError');
 
 /**
@@ -34,7 +34,21 @@ const successfulPayment = async (publicWebhookBody) => {
   }
 };
 
+/**
+ * Create a publicWebhook record
+ * @param {Object} publicWebhookBody
+ * @returns {Promise<PublicWebhook>}
+ */
+const failedPayment = async (publicWebhookBody) => {
+  try {
+    return await PublicWebhookPaymentFailed.create(publicWebhookBody);
+  } catch (error) {
+    throw new ApiError(httpStatus.BAD_REQUEST, error);
+  }
+};
+
 module.exports = {
   successfulSubscription,
   successfulPayment,
+  failedPayment,
 };
