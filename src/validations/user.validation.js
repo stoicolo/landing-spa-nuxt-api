57774c33b503668a9446aa9cabbd962249cab6a0
francomac <franco.mcarballo@outlook.com>
2024-10-01 19:15:29 -0600

    refactor(User): adding register validation for User controller

--- src/validations/user.validation.js
+++ src/validations/user.validation.js
@@ -1,12 +1,22 @@
 const Joi = require('joi');
 const { password, objectId } = require('./custom.validation');
 
-const createUser = {
+const register = {
   body: Joi.object().keys({
-    email: Joi.string().required().email(),
-    password: Joi.string().required().custom(password),
-    name: Joi.string().required(),
-    role: Joi.string().required().valid('user', 'admin'),
+    name: Joi.string().required().min(2).max(50).trim().messages({
+      'string.min': 'El nombre debe tener al menos 2 caracteres',
+      'string.max': 'El nombre no puede exceder los 50 caracteres',
+      'any.required': 'El nombre es requerido',
+    }),
+    email: Joi.string().required().email().lowercase().trim().messages({
+      'string.email': 'Debe ser un email válido',
+      'any.required': 'El email es requerido',
+    }),
+    password: Joi.string().required().custom(password).messages({
+      'any.required': 'La contraseña es requerida',
+    }),
+    phoneNumber: Joi.string().required(),
+    role: Joi.string().required().valid('user'),
   }),
 };
 
@@ -27,14 +37,13 @@ const getUser = {
 };
 
 const updateUser = {
-  params: Joi.object().keys({
-    userId: Joi.required().custom(objectId),
-  }),
   body: Joi.object()
     .keys({
+      id: Joi.number().integer(),
+      name: Joi.string(),
       email: Joi.string().email(),
       password: Joi.string().custom(password),
-      name: Joi.string(),
+      phoneNumber: Joi.string(),
     })
     .min(1),
 };
@@ -46,7 +55,7 @@ const deleteUser = {
 };
 
 module.exports = {
-  createUser,
+  register,
   getUsers,
   getUser,
   updateUser,
