dddcbcba1ae67c52009b2092c69f4e2656beb1f2
francomac <franco.mcarballo@outlook.com>
2024-10-01 19:16:44 -0600

    refactor(Auth): using new register controller method and validation method

--- src/routes/v1/auth.route.js
+++ src/routes/v1/auth.route.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const validate = require('../../middlewares/validate');
 const authValidation = require('../../validations/auth.validation');
+const userValidation = require('../../validations/user.validation');
 const authController = require('../../controllers/auth.controller');
+const userController = require('../../controllers/user.controller');
 const auth = require('../../middlewares/auth');
 
 const router = express.Router();
 
-router.post('/register', validate(authValidation.register), authController.register);
+router.post('/register', validate(userValidation.register), userController.register);
 router.post('/login', validate(authValidation.login), authController.login);
 router.post('/logout', validate(authValidation.logout), authController.logout);
 router.post('/refresh-tokens', validate(authValidation.refreshTokens), authController.refreshTokens);
