48d17012b09828daf9208fef64883a3152bf8cf5
francomac <franco.mcarballo@outlook.com>
2024-10-05 13:13:27 -0600

    feature(legalAgreements): new models

---
+++ src/controllers/legalAgreement.controller.js
@@ -0,0 +1,69 @@
+const httpStatus = require('http-status');
+const pick = require('../utils/pick');
+const ApiError = require('../utils/ApiError');
+const catchAsync = require('../utils/catchAsync');
+const { userService, tokenService, emailService } = require('../services');
+const { tokenTypes } = require('../config/tokens');
+const logger = require('../config/logger');
+
+/**
+ * Register a new user
+ * @param {Object} req - Express request object
+ * @param {Object} res - Express response object
+ */
+const register = catchAsync(async (req, res) => {
+  const user = await userService.createUser(req.body);
+  const tokens = await tokenService.generateAuthTokens(user, tokenTypes.VERIFY_EMAIL);
+  const response = {
+    user: {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      role: user.role,
+    },
+    tokens,
+  };
+  await emailService.sendEmailActivation(response.user, response.tokens);
+  logger.info(`User registered: ${user.email}`);
+  res.status(httpStatus.CREATED).send(response);
+});
+
+const getUsers = catchAsync(async (req, res) => {
+  const filter = pick(req.query, ['name', 'role']);
+  const options = pick(req.query, ['sortBy', 'limit', 'page']);
+  const result = await userService.queryUsers(filter, options);
+  res.send(result);
+});
+
+const getUser = catchAsync(async (req, res) => {
+  const user = await userService.getUserById(req.params.userId);
+  if (!user) {
+    throw new ApiError(httpStatus.NOT_FOUND, 'Usuario no encontrado, verifica el id.');
+  }
+  res.send(user);
+});
+
+const updateUser = catchAsync(async (req, res) => {
+  const user = await userService.updateUserById(req.body.id, req.body);
+  res.send(user);
+});
+
+const deleteUser = catchAsync(async (req, res) => {
+  await userService.deleteUserById(req.params.userId);
+  res.status(httpStatus.NO_CONTENT).send();
+});
+
+const sendContactFormResponseEmail = catchAsync(async (req, res) => {
+  await emailService.sendContactFormResponseEmail(req.body);
+  logger.info('Email sent successfully');
+  res.status(httpStatus.NO_CONTENT).send();
+});
+
+module.exports = {
+  register,
+  getUsers,
+  getUser,
+  updateUser,
+  deleteUser,
+  sendContactFormResponseEmail,
+};
